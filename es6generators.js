c35ef20788d009fd1a75f785a586d2ec39177801
John Son <jsson77@gmail.com>
2017-03-15 11:01:26 +0100

    implemented co npm package for coroutines for generator function

--- es6generators.js
+++ es6generators.js
@@ -1,19 +1,20 @@
-const url =
-'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
-const fetch = require('node-fetch')
-
-function* createQuoteFetcher() {
-  const response = yield fetch(url)
-  const quote = yield response.json()
-  return `${quote.quoteText} -${quote.quoteAuthor}`
-}
-
-const quoteFetcher = createQuoteFetcher()
-quoteFetcher.next().value
-  .then(res => quoteFetcher.next(res).value)
-  .then(res => quoteFetcher.next(res).value)
-  .then(quote => console.log(quote))
-  .catch(err => console.log(err))
+//  PLAIN CODE
+  // const url =
+  // 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+  // const fetch = require('node-fetch')
+  //
+  // function* createQuoteFetcher() {
+  //   const response = yield fetch(url)
+  //   const quote = yield response.json()
+  //   return `${quote.quoteText} -${quote.quoteAuthor}`
+  // }
+  //
+  // const quoteFetcher = createQuoteFetcher()
+  // quoteFetcher.next().value
+  //   .then(res => quoteFetcher.next(res).value)
+  //   .then(res => quoteFetcher.next(res).value)
+  //   .then(quote => console.log(quote))
+  //   .catch(err => console.log(err))
 
 //  CODE WITH COROUTINE IMPLEMENTATION
   // const url =
@@ -42,3 +43,20 @@ quoteFetcher.next().value
   // quoteFetcher
   //   .then(quote => console.log(quote))
   //   .catch(err => console.log(err))
+
+//  CODE WITH NPM CO
+  const url =
+  'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+  const fetch = require('node-fetch')
+  const co = require('co')
+
+  function* createQuoteFetcher() {
+    const response = yield fetch(url)
+    const quote = yield response.json()
+    return `${quote.quoteText} -${quote.quoteAuthor}`
+  }
+
+  const quoteFetcher = co(createQuoteFetcher)
+  quoteFetcher
+    .then(quote => console.log(quote))
+    .catch(err => console.log(err))
