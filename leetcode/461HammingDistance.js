a0c0c20654806dec998299ce0ef4ce695e170d0f
John Son <jsson77@gmail.com>
2017-04-05 10:47:46 +0200

    461 hamming distance 2

--- leetcode/461HammingDistance.js
+++ leetcode/461HammingDistance.js
@@ -7,28 +7,22 @@ var hammingDistance = function(x, y) {
     let xS = x.toString(2), xLen = xS.length
     let yS = y.toString(2), yLen = yS.length
 
+    let diffs = 0
+    let max = Math.max(xLen, yLen)
+    max === xLen ? (ans = xLen - yLen) : (ans = yLen - xLen)
 
-    let xCount = 0
-    let yCount = 0
+    console.log('xS: ', xS, 'yS: ', yS, 'max: ', max)
 
-    console.log('xS: ', xS, 'yS: ', yS)
-
-    // for (let i = xS.length; i >= 0; i--) {
-    //     xCount++
-    // }
+    for (let i = xS.length; i >= 0; i--) {
+        xCount++
+    }
 
     // for (let i = yS.length; i >= 0; i--) {
     //     yCount++
     // }
 
-    let max = Math.max(xLen, yLen)
-    let ans
 
-    max === xLen ? (ans = xLen - yLen) : (ans = yLen - xLen)
-
-    return ans
-
-
+    return diffs
 };
 
 let x1 = 93, y1 = 73
